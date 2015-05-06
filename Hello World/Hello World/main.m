114757cdea5f7c4c633e30acc13d3c6cd6151173
Angel Baek <angelbaek1@gmail.com>
2015-05-06 15:17:22 -0400

    start hello world proj/tutorial

---
+++ Hello World/Hello World/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by Angel Baek on 5/6/15.
+//  Copyright (c) 2015 Angel Baek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        int age = 30;
+        NSLog(@"Your age is %i", age);
+        
+        
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
